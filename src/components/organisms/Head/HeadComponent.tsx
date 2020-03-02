3bc158465d2a259b9f5804e1d18d39aa8dc60314
Bruno Cabral <bruno.poc@gmail.com>
2020-03-02 20:06:02 -0300

    add images metatags

--- src/components/organisms/Head/HeadComponent.tsx
+++ src/components/organisms/Head/HeadComponent.tsx
@@ -38,10 +38,14 @@ export default ({ title, description, url, image }: HeadProps) => (
     <meta property="og:title" content={title} />
     <meta property="og:type" content="article" />
     <meta property="og:url" content={url} />
-    <meta property="og:image" content={image} />
+    <meta property="og:image" content={image.replace(/^https:\/\//i, 'http://')} />
+    <meta property="og:image:secure_url" content={image} />
+    <meta property="og:image:type" content="image/jpeg" />
+    <meta property="og:image:width" content="600" />
     <meta property="og:image:alt" content={`image = ${title}`} />
     <meta property="og:site_name" content="Sou Gamer Com Orgulho" />
     <meta property="og:description" content={description} />
+    <meta itemProp="image" content={image} />
     <meta name="tweetmeme-title" content={title} />
   </Head>
 );
