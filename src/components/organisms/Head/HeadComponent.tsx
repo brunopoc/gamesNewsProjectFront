34ed57806f4f9af2a6e547d0eb8dea7f80cd367e
Bruno Cabral <bruno.poc@gmail.com>
2020-03-02 20:19:33 -0300

    set image height metatag

--- src/components/organisms/Head/HeadComponent.tsx
+++ src/components/organisms/Head/HeadComponent.tsx
@@ -37,11 +37,12 @@ export default ({ title, description, url, image }: HeadProps) => (
     <meta httpEquiv="Cache-Control" content="no-cache" />
     <meta property="og:title" content={title} />
     <meta property="og:type" content="article" />
-    <meta property="og:url" content={url} />
     <meta property="og:image" content={image.replace(/^https:\/\//i, 'http://')} />
+    <meta property="og:image:url" content={image.replace(/^https:\/\//i, 'http://')} />
     <meta property="og:image:secure_url" content={image} />
     <meta property="og:image:type" content="image/jpeg" />
-    <meta property="og:image:width" content="600" />
+    <meta property="og:image:width" content="400" />
+    <meta property="og:image:height" content="300" />
     <meta property="og:image:alt" content={`image = ${title}`} />
     <meta property="og:site_name" content="Sou Gamer Com Orgulho" />
     <meta property="og:description" content={description} />
